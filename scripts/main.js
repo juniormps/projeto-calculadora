aaa61b419d49d0a6f7614baa3a998d981a4e9f5f
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-10-25 20:17:37 -0300

    implementação da funcionalidade de imprimir no visor
    imprime o valor da tecla digitada no visor da calculadora.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,32 @@
 //Faz com que cada número ou operação teclado, seja visualizado no visor
-function historicoOperacoes(botao){
+
+const historico = document.querySelector("#historico") 
+
+const botoes = document.querySelectorAll("[tecla-display]")
+
+let ultimoValor = ""
+
+function imprimirNoHistorico(event) {
+    const valorTecla = event.target.innerText
+
+    if (!isNaN(valorTecla)) {
+        historico.innerHTML += valorTecla;
+        ultimoValor = valorTecla
+
+    } else if ("+-÷x".includes(valorTecla)) {
+
+        if (!"+-÷x".includes(ultimoValor)) {
+            historico.innerHTML += valorTecla
+            ultimoValor = valorTecla
+        }
+    } else if (valorTecla === ".") {
     
+        if (!ultimoValor.includes(".")) {
+            historico.innerHTML += valorTecla
+            ultimoValor += valorTecla
+        }
+    }
+
 }
+
+botoes.forEach(botao => botao.onclick = imprimirNoHistorico)
